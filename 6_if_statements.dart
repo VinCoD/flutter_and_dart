d2e699ff4cb84c09780a834ddf2ccda200d14df6
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-25 10:37:41 +0300

    Switch cases and if statements done

---
+++ 6_if_statements.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 17;
+  print('the user is $age years');
+
+  if (age >= 18) {
+    print("you can enter the movie");
+  } else if (age >= 16) {
+    print("You can enter the movie with your parent");
+  } else {
+    print("Fuck off");
+  }
+}
---
+++ 7_switch_statement.dart
@@ -0,0 +1,14 @@
+void main() {
+  final String name = "Ali";
+
+  switch (name) {
+    case "Vincent":
+      print("Your name is Vincent");
+      break;
+    case "Peter":
+      print("Its you we are looking for $name");
+      break;
+    default:
+      print("I don't know my name");
+  }
+}
