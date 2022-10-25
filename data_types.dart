e221f72b672cc17e27f8c0e166498608a3b9c7fa
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-25 08:48:11 +0300

    variables and datatypes

---
+++ data_types.dart
@@ -0,0 +1,11 @@
+void main() {
+  bool boolOutput = true;
+  print(boolOutput);
+  int intOutput = -1;
+  double doubleOutput = 1.5;
+  print(intOutput);
+  print(doubleOutput);
+  String stringOutput = 'Hello, Felix';
+  print(stringOutput);
+  print('the value is $intOutput');
+}
---
+++ variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  int outputInt = 2;
+  print('outputInt: $outputInt');
+  late int lateOutputInt;
+  lateOutputInt = 42;
+
+  print("lateOutputInt: $lateOutputInt");
+}
