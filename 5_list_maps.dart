9bc179cea1ee2868116792257f7ddd91be1b36fe
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-25 10:17:43 +0300

    Maps done

--- 5_list_maps.dart
+++ 5_list_maps.dart
@@ -1,8 +1,10 @@
 void main() {
+  // Lists
   List exampleList = [1, 2, 3, 4];
   print(exampleList);
   print(exampleList[3]);
 
+  // Lists with specific type
   List<int> exampleList2 = [1, 2, 4, 3];
   print(exampleList2[3]);
 
@@ -13,6 +15,8 @@ void main() {
   print(siblings[3]);
 
   List<dynamic> exampleList4 = [1, 2, 3, "Vincent", "Old", 2.1];
+
+  // List Methods
   print(exampleList4.length);
   print(exampleList4.first);
   print(exampleList4.isEmpty);
@@ -20,4 +24,22 @@ void main() {
   print(exampleList4.last);
   exampleList4.add("value");
   print(exampleList4);
+
+  // Maps
+
+  Map exampleMap = {
+    'key1': 'value1',
+    'key2': 'value2',
+  };
+
+  print(exampleMap['key1']);
+
+  // Maps with specific type
+
+  Map<String, String> exampleMap2 = {
+    'key1': 'value1',
+    'key2': 'value2',
+  };
+  
+  print(exampleMap2['key2']);
 }
