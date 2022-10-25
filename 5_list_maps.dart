c0035695f50d4c44a56bebf2e6f1a5b36ce51e17
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-25 09:43:42 +0300

    List understood

---
+++ 5_list_maps.dart
@@ -0,0 +1,14 @@
+void main() {
+  List exampleList = [1, 2, 3, 4];
+  print(exampleList);
+  print(exampleList[3]);
+
+  List<int> exampleList2 = [1, 2, 4, 3];
+  print(exampleList2[3]);
+
+  List<double> exampleList3 = [1, 2.4, 4, 3];
+  print(exampleList3[3]);
+
+  List<String> siblings = ["Vincent", "Maurice", "Liz", "Stephen"];
+  print(siblings[3]);
+}
