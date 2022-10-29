fad7d89b5bf0fd3860441640cad805641fb02cbd
VinCoD <5895.2020@students.ku.ac.ke>
2022-10-29 08:02:45 +0300

    Started assynchronous programming

---
+++ 17_assynchronous_programming.dart
@@ -0,0 +1,9 @@
+Future<void> fetchUserOrder() {
+  // Imagine that this function is fetching user info from another service or database.
+  return Future.delayed(const Duration(seconds: 2), () => print('Large Latte'));
+}
+
+void main() {
+  fetchUserOrder();
+  print('Fetching user order...');
+}
