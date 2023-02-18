6fdec865dd00a132d8667a53f87b180b96dd7d90
VinCoD <5895.2020@students.ku.ac.ke>
2023-02-18 22:47:06 +0300

    Variables Declaration

--- 2_variables.dart
+++ 2_variables.dart
@@ -5,4 +5,22 @@ void main() {
   lateOutputInt = 42;
 
   print("lateOutputInt: $lateOutputInt");
+  // Declared without specifying the type - Infers type
+var filter = 'company';
+// Declared by type
+String filter = 'company';
+// Uninitialized variable has an initial value of null
+String filter;
+// Value will not change
+final filter = 'company';
+// or
+final String filter = 'company';
+// or
+const String filter = 'company';
+// or
+const String filter = 'company' + filterOption;
+// Public variable (variable name starts without underscore)
+String userName = 'Sandy';
+// Private variable (variable name starts with underscore)
+String _userID = 'XW904';
 }
